493ffd568f4ceaf7de0cecc14a7861ae2848c3e8
Enric Bisbe Gil <enricu@gmail.com>
2021-10-19 18:55:01 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main-header />
+  <main-stats />
+  <main-grid />
+  <main-testimonial />
+  <main-features />
+  <main-cta />
+  <main-blog />
+  <main-footer />
+</template>
+
+
---
+++ src/components/MainStats.vue
@@ -0,0 +1,47 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="bg-indigo-800">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
+      <div class="max-w-4xl mx-auto text-center">
+        <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+          Skedr users boosting their productivity
+        </h2>
+        <p class="mt-3 text-xl text-indigo-200 sm:mt-4">
+          Spending less time sharing photos to Flickr groups.
+        </p>
+      </div>
+      <dl
+        class="
+          mt-10
+          text-center
+          sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8
+        "
+      >
+        <div class="flex flex-col">
+          <dt
+            class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
+          >
+            tagged groups
+          </dt>
+          <dd class="order-1 text-5xl font-extrabold text-white">40.989</dd>
+        </div>
+        <div class="flex flex-col mt-10 sm:mt-0">
+          <dt
+            class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
+          >
+            scheduled images
+          </dt>
+          <dd class="order-1 text-5xl font-extrabold text-white">750.671</dd>
+        </div>
+        <div class="flex flex-col mt-10 sm:mt-0">
+          <dt
+            class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
+          >
+            shared images
+          </dt>
+          <dd class="order-1 text-5xl font-extrabold text-white">4.629.348</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</template>
