f083957fd8c387207800015dd07760f476438999
Enric Bisbe Gil <enricu@gmail.com>
2021-11-09 18:21:00 +0100

    Add G Tag

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { ViteSSG } from "vite-ssg"
 import App from "./App.vue"
 import "./index.css"
 import { routes } from "./routes"
+import VueGtag from "vue-gtag"
 
-export const createApp = ViteSSG(App, { routes })
+export const createApp = ViteSSG(App, { routes }, ({ app, router }) => {
+  app.use(
+    VueGtag,
+    {
+      config: {
+        id: import.meta.env.VITE_GTAG
+      }
+    },
+    router
+  )
+})
