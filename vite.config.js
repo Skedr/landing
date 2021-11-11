49c50900d116803c34f3f9ddd74af7453d521674
Enric Bisbe Gil <enricu@gmail.com>
2021-11-11 19:14:32 +0100

    Add vite-plugin-imagemin to compress images

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,40 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import Components from "unplugin-vue-components/vite"
 import eslintPlugin from "vite-plugin-eslint"
+import viteImagemin from "vite-plugin-imagemin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({}), eslintPlugin()]
+  plugins: [
+    vue(),
+    Components({}),
+    eslintPlugin(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox"
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false
+          }
+        ]
+      }
+    })
+  ]
 })
