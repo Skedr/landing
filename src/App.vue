6ecd827720efd872d6a36153f8f31975dc0245a5
Enric Bisbe Gil <enricu@gmail.com>
2021-11-18 13:21:26 +0100

    Load helpscount once the App has been loaded

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,34 @@
   import { useHead } from "@vueuse/head"
   import { useRoute } from "vue-router"
 
+  const loadHelpScout = () => {
+    !(function (e, t, n) {
+      function a() {
+        var e = t.getElementsByTagName("script")[0],
+          n = t.createElement("script")
+        ;(n.type = "text/javascript"),
+          (n.async = !0),
+          (n.src = "https://beacon-v2.helpscout.net"),
+          e.parentNode.insertBefore(n, e)
+      }
+      if (
+        ((e.Beacon = n =
+          function (t, n, a) {
+            e.Beacon.readyQueue.push({ method: t, options: n, data: a })
+          }),
+        (n.readyQueue = []),
+        "complete" === t.readyState)
+      )
+        return a()
+      e.attachEvent ? e.attachEvent("onload", a) : e.addEventListener("load", a, !1)
+    })(window, document, window.Beacon || function () {})
+  }
+
   export default defineComponent({
     setup() {
       const route = useRoute()
-
+      loadHelpScout()
+      window.Beacon("init", "9d640825-f226-46b1-9a92-dbed14392554")
       useHead({
         title: computed(() => route.meta.title),
         meta: [
