cd21e4e05e619e9e0338e21cf376d3d12dfa1440
Enric Bisbe Gil <enricu@gmail.com>
2024-02-01 18:44:29 +0100

    auto generate sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()],
+  integrations: [vue(), tailwind(), sitemap()],
+  site: "https://skedr.io",
 });
