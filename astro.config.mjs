4b0495e14fe8530210591c4f6b7ef2be4130916e
Enric Bisbe Gil <enricu@gmail.com>
2024-02-01 20:16:53 +0100

    add partytown  for GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), sitemap()],
+  integrations: [
+    vue(),
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "fbq"],
+      },
+    }),
+  ],
   site: "https://skedr.io",
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_GTM: string;
+  readonly PUBLIC_URL: string;
+}
