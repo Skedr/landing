266f6953b85c56e98a0a70e95c270aafc21128b8
Enric Bisbe Gil <enricu@gmail.com>
2024-02-01 12:54:03 +0100

    clear code

--- src/routes.js
+++ src/routes.js
@@ -1,58 +1,5 @@
 export const routes = [
   {
-    path: "/",
-    name: "home",
-    components: {
-      default: () => import("./views/HomePage.vue"),
-      header: () => import("./components/headers/HeaderHome.astro")
-    },
-    
-  },
-  {
-    path: "/pricing",
-    name: "pricing",
-    components: {
-      default: () => import("./pages/PricingPage.vue"),
-      header: () => import("./components/headers/HeaderPricing.astro")
-    },
-    
-  },
-  {
-    path: "/features",
-    name: "features",
-    components: {
-      default: () => import("./views/FeaturesPage.vue"),
-      header: () => import("././components/headers/HeaderFeatures.vue")
-    },
-    
-  },
-  {
-    path: "/faq",
-    name: "faq",
-    components: {
-      default: () => import("./views/FaqPage.vue"),
-      header: () => import("./components/headers/HeaderFaq.vue")
-    },
-    
-  },
-  {
-    path: "/privacy-policy",
-    name: "privacy",
-    components: {
-      default: () => import("./views/PrivacyPolicyPage.vue"),
-      header: () => import("./components/headers/HeaderPrivacyPolicy.astro")
-    },
-  },
-  {
-    path: "/terms-and-conditions",
-    name: "terms",
-    components: {
-      default: () => import("./views/TermsAndConditionsPage.vue"),
-      header: () => import("./components/headers/HeaderTermsAndConditions.vue")
-    },
-    
-  },
-  {
     path: "/articles/group-tagging",
     name: "tags",
     components: {
