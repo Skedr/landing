8b3dacff35f375dc3e918bf90c68eb8e0778cc82
Enric Bisbe Gil <enricu@gmail.com>
2024-02-07 17:44:52 +0100

    remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
