8a6a806373576851bb54137590555619277b0049
Enric Bisbe Gil <enricu@gmail.com>
2024-03-27 17:57:09 +0100

    refactor to satisfing results

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,14 +5,11 @@ const postsCollection = defineCollection({
     type: 'content',
     schema: z.object({
       title: z.string(),
+      pretitle: z.string(),
       pubDate: z.date(),
       description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
+      tags: z.array(z.string()),
+      time: z.number()
     })
 });
 // Export a single `collections` object to register your collection(s)
