8896fa6014afb0f62e8c473c78f87f279e0ac653
Enric Bisbe Gil <enricu@gmail.com>
2024-03-27 15:30:42 +0100

    use base markdown in post collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import rehypeFigure from "rehype-figure";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind(), sitemap()],
   site: "https://skedr.io",
   image: {
-    domains: ['images.unsplash.com']
-  }
+    domains: ["images.unsplash.com"],
+  },
+  markdown: {
+    rehypePlugins: [rehypeFigure],
+  },
 });
