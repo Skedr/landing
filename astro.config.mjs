ac05cfc2f0c563ab8ff278344d6c5cd379e54fcd
Enric Bisbe Gil <enricu@gmail.com>
2024-03-27 16:23:03 +0100

    blog post first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import rehypeFigure from "rehype-figure";
+import rehypeFigure from "rehype-figure"
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), sitemap()],
+  integrations: [vue(), tailwind(), sitemap(), markdoc({allowHTML: true})],
   site: "https://skedr.io",
   image: {
     domains: ["images.unsplash.com"],
