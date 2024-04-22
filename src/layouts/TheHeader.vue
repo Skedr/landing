1e380bebd842c13db1b9b454093376d05a0e12e8
Enric Bisbe Gil <enricu@gmail.com>
2024-04-22 16:40:36 +0200

    fix: show blog on nabvar

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -16,7 +16,7 @@ const navigation = [
   { name: "Home", href: "/" },
   { name: "Features", href: "/features" },
   { name: "Pricing", href: "/pricing" },
-  // { name: "Blog", href: "/blog" },
+  { name: "Blog", href: "/blog" },
   { name: "FAQ", href: "/faq" },
 ];
 
