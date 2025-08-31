aff8b44d635e9fdb28f2489018d14e7df18fa882
Enric Bisbe Gil <enricu@gmail.com>
2025-08-31 12:04:33 +0200

    fix: throw error if user is delted from flickr

--- src/utils/flickrHelper.ts
+++ src/utils/flickrHelper.ts
@@ -3,11 +3,14 @@ import { createFlickr } from "flickr-sdk";
 const { flickr } = createFlickr(import.meta.env.PUBLIC_FLICKR_KEY);
 
 export const getPeopleProfile = async (userId: string) => {
-  const { person } = await flickr("flickr.people.getInfo", {
-    user_id: userId,
-  });
+  try {
+    const { person } = await flickr("flickr.people.getInfo", {
+      user_id: userId,
+    });
+    const profileUrl = `https://farm${person.iconfarm}.staticflickr.com/${person.iconserver}/buddyicons/${userId}_r.jpg`;
 
-  const profileUrl = `https://farm${person.iconfarm}.staticflickr.com/${person.iconserver}/buddyicons/${userId}_r.jpg`;
-
-  return { person, profileUrl };
+    return { person, profileUrl };
+  } catch (error) {
+    throw new Error(`Error getting people profile for ${userId}`);
+  }
 };
